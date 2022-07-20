7db8968f29257588b74169dd3b0d2f7d836ef72b
0xph03n1x <lazarovmihail97@gmail.com>
2022-07-20 17:53:17 +0300

    Integers and comments

---
+++ integers_and_comments.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int var1 = 35;  //First variable
+    int var2 = 12;  //Second variable
+
+    /*
+    Combine the two variables and print the result
+    */
+
+    std::cout << "Combination of var1 and var2 equals: " << var1 + var2;
+}
