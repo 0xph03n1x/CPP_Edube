6f14658a53cf2d6ae5754df01f4c4802e19ee01b
0xph03n1x <lazarovmihail97@gmail.com>
2022-07-23 16:32:38 +0300

    In n out v2

--- in_n_out.cpp
+++ in_n_out.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(void)
 {
     int byte = 255;
-    cout << "Bytes in hex: " << hex /*This can be either hex < dec < byte value*/ << byte;
+    cout << "Bytes in hex: " << hex /*This can be either hex < dec < byte value*/ << byte << "\n";
+
+    /*
+    Requires the iomanip header file.
+    Using the setbase manipulator.
+    The only acceptable values for the setbase parameter are 8, 10, 16.
+    */
+
+    cout << "Bytes in hex with setbase: " << setbase(16) << byte << "\n";
+    return 0;
 }
